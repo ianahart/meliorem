f7b933e223df37bc17d85059dd9dce8bd9eb67bc
Ian Hart <ianhart7790@gmail.com>
2024-01-05 13:50:44 -0500

    feat: initial commit

---
+++ src/main/java/com/hart/meliorem/MelioremApplication.java
@@ -0,0 +1,13 @@
+package com.hart.meliorem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelioremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelioremApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hart/meliorem/MelioremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.meliorem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelioremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
