ea937a363e8f7b7c90d49c2d97726babe0d15dac
Ian Hart <ianhart7790@gmail.com>
2024-01-28 11:14:07 -0500

    feat: a user can now get all study sets on the latest page

--- frontend/src/components/Latest/LatestContainer.tsx
+++ frontend/src/components/Latest/LatestContainer.tsx
@@ -6,14 +6,7 @@ interface ILatestContainerProps {
 
 const LatestContainer = ({ children }: ILatestContainerProps) => {
   return (
-    <Box
-      as="section"
-      w="100%"
-      maxW="960px"
-      border="1px solid pink"
-      mx="auto"
-      minH="100vh"
-    >
+    <Box as="section" w="100%" maxW="960px" mx="auto" minH="100vh">
       {children}
     </Box>
   );
--- frontend/src/components/Latest/index.tsx
+++ frontend/src/components/Latest/index.tsx
@@ -1,12 +1,16 @@
 import { Box } from '@chakra-ui/react';
 import LatestContainer from './LatestContainer';
 import YourStudySets from './YourStudySets';
+import AllStudySets from './AllStudySets';
 
 const Latest = () => {
   return (
     <LatestContainer>
       <Box my="2rem" as="section">
         <YourStudySets />
+      </Box>
+      <Box my="2rem" as="section">
+        <AllStudySets />
       </Box>
     </LatestContainer>
   );
