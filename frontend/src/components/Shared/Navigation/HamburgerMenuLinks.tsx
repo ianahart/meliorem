44bcdb28efc0d3c5f737c6f8248e00d50ed1f663
Ian Hart <ianhart7790@gmail.com>
2024-02-19 15:10:00 -0500

    chore: remove unused imports

--- frontend/src/components/Shared/Navigation/HamburgerMenuLinks.tsx
+++ frontend/src/components/Shared/Navigation/HamburgerMenuLinks.tsx
@@ -2,27 +2,18 @@ import { Box } from '@chakra-ui/react';
 import HamburgerMenuLink from './HamburgerMenuLink';
 import HamburgerMenuLinkMin from './HamburgerMenuLinkMin';
 import { AiFillEdit } from 'react-icons/ai';
-import { CgNotes } from 'react-icons/cg';
 import { PiCards } from 'react-icons/pi';
-import { FaRegFolder } from 'react-icons/fa';
 
 export interface IHamburgerMenuLinksProps {
   closeHamburgerMenu: () => void;
 }
 
 const HamburgerMenuLinks = ({ closeHamburgerMenu }: IHamburgerMenuLinksProps) => {
-  const createSubLinks = [
-    { linkName: '/study-set', linkText: 'Study set', icon: <PiCards /> },
-  ];
+  const createSubLinks = [{ linkName: '/study-set', linkText: 'Study set', icon: <PiCards /> }];
 
   return (
     <Box color="primary.light">
-      <HamburgerMenuLinkMin
-        closeHamburgerMenu={closeHamburgerMenu}
-        linkPath="/"
-        linkName="Home"
-        icon="M"
-      />
+      <HamburgerMenuLinkMin closeHamburgerMenu={closeHamburgerMenu} linkPath="/" linkName="Home" icon="M" />
       <HamburgerMenuLink
         closeHamburgerMenu={closeHamburgerMenu}
         linkName="Create"
